c406533092f4e4b8f953716ac29691aff0796444
Lizandro Mayonado <lmayonado.cloudpanda@gmail.com>
2021-09-15 20:58:35 +0800

    UPDATE: added header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,12 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
+        <meta
+          name="description"
+          content="A web application that collates the latest articles from Dev.to & daily trending repositories from Github filtered in Javascript programming language."
+        />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/250x120.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>CLIPNOTES</title>
       </Head>
       <div className="scrollbar scrollbar-thin scrollbar-thumb-gray-400 bg-gray-50 min-h-screen flex flex-col">
