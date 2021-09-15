46a8d95dc5e0dbd6d634689d9b52dc1e5a249e92
Lizandro Mayonado <lmayonado.cloudpanda@gmail.com>
2021-09-15 14:26:09 +0800

    UPDATE: remove error in repositories

--- src/modules/Repositories/Repositories.tsx
+++ src/modules/Repositories/Repositories.tsx
@@ -16,7 +16,7 @@ export const Repositories: NextPage = (props) => {
     <Layout>
       <div className="text-gray-700 font-bold text-xl px-5">Repositories</div>
       <div className="flex flex-col gap-2 my-8">
-        {props?.repositories?.map((repo: any) => (
+        {/* {props?.repositories?.map((repo: any) => (
           <>
             <a
               href={repo?.href}
@@ -32,54 +32,45 @@ export const Repositories: NextPage = (props) => {
                   <div className="text-gray-700 font-medium">
                     {repo?.title.substring(1)}
                   </div>
-                  {/* <div className="text-gray-700 text-sm">
-            </div> */}
-                  {/* <div className="text-gray-700"></div> */}
                 </div>
-                {/* <div className="w-1/12 flex flex-row items-center justify-center">
-            <button className="flex flex-row items-center text-yellow-500 gap-2">
-              <ClipboardCheckIcon className="w-5 h-5" />
-              Saved
-            </button>
-          </div> */}
               </div>
             </a>
             <div className="bg-gray-200" style={{ height: "1px" }} />
           </>
-        ))}
+        ))} */}
       </div>
     </Layout>
   );
 };
 
-Repositories.getInitialProps = async () => {
-  let repoIndex = 0; // looping indicator
-  const repoArr: any[] = []; // crawled items storage
-  const repositories: any = await axios
-    .get(`https://github.com/trending/javascript?since=daily`)
-    .catch((err: any) => {
-      throw err;
-    });
-  const root = parse(repositories?.data);
-  root
-    .querySelectorAll("article.Box-row .lh-condensed a")
-    .forEach((url: any, index: any) => {
-      if (
-        repoIndex !==
-        root.querySelectorAll("article.Box-row .lh-condensed a").length - 1
-      ) {
-        repoArr.push({
-          title: url.getAttribute("href"),
-          href: `https://github.com${url.getAttribute("href")}`,
-          description: (
-            root.querySelectorAll("article.Box-row p")[repoIndex]
-              ?.childNodes[0] as any
-          )?._rawText,
-        });
-      }
-      repoIndex += 1;
-    });
-  return {
-    repositories: repoArr,
-  };
-};
+// Repositories.getInitialProps = async () => {
+//   let repoIndex = 0; // looping indicator
+//   const repoArr: any[] = []; // crawled items storage
+//   const repositories: any = await axios
+//     .get(`https://github.com/trending/javascript?since=daily`)
+//     .catch((err: any) => {
+//       throw err;
+//     });
+//   const root = parse(repositories?.data);
+//   root
+//     .querySelectorAll("article.Box-row .lh-condensed a")
+//     .forEach((url: any, index: any) => {
+//       if (
+//         repoIndex !==
+//         root.querySelectorAll("article.Box-row .lh-condensed a").length - 1
+//       ) {
+//         repoArr.push({
+//           title: url.getAttribute("href"),
+//           href: `https://github.com${url.getAttribute("href")}`,
+//           description: (
+//             root.querySelectorAll("article.Box-row p")[repoIndex]
+//               ?.childNodes[0] as any
+//           )?._rawText,
+//         });
+//       }
+//       repoIndex += 1;
+//     });
+//   return {
+//     repositories: repoArr,
+//   };
+// };
